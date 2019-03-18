2e9c97dfb05586727eafd5308264717094774eaf
suwanna <student05@Student05.local>
2019-03-18 15:03:29 +0700

    start

--- DataTypeOfVariable.playground/Contents.swift
+++ DataTypeOfVariable.playground/Contents.swift
@@ -5,4 +5,26 @@ import UIKit
 var myName = "Doremon" //นี่คือการประกาศ ที่อาศัยชนิดของ Value  บอก Datatype
 var mySurname: String = "AAAAAA" //นี่คือการประกาศแบบ กำหนด Datatype ตั้งแต่เกิด
 
+//การแสดงผลบน Cosole
+print("This is Console")
+print("myName = \(myName)")
+
+
+//intger Type หรือ จำนวนที่ไม่มีเศษ
+var number1 = 100
+var number2: Int = 200
+
+//Double Type หรือ จำนวนที่มีเศษ
+var number3 = 3.1416
+var number4: Double = 10.234
+
+//boolean Type คือจำนวนที่มีแค่ค่า true,false
+var status = true
+var status2:Bool = false
+
+
+
+
+
+
 
