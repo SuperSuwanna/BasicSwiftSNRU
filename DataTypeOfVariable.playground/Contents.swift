b1f8d75da7dba5505283e2e264b2eecfebf62452
suwanna <student05@Student05.local>
2019-03-18 14:30:06 +0700

    start Data type

---
+++ DataTypeOfVariable.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+//String Type ตัวแปรที่เป็นอักษร
+
+var myName = "Doremon" //นี่คือการประกาศ ที่อาศัยชนิดของ Value  บอก Datatype
+var mySurname: String = "AAAAAA" //นี่คือการประกาศแบบ กำหนด Datatype ตั้งแต่เกิด
+
+
---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//Explicit Variabip
+//การประกาศตัววแปร
+var  myName = "Suwanna"
+myName="Doramon"
+
+//Expplicit Conatant
+let myFriend = "Nopita"
+//myFriend = "Sunako"Error Because myfriend is let
+
+
